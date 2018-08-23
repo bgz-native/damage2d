3f9386eba0a82b0151690cd97382d091b2d15a64
Martin Garcia <m.garcia@tose.com.ph>
2018-08-23 15:36:40 +0800

    + added vector4 to math for rgba support

--- DaMaGE/src/vector.h
+++ DaMaGE/src/vector.h
@@ -67,6 +67,38 @@ namespace dmg { namespace math {
         float       x, y, z;
     };
 
+    class vector4
+    {
+    public:
+        vector4() { zero(); }
+        ~vector4() {}
+
+        vector4(float p_x, float p_y, float p_z, float p_a) : x(p_x), y(p_y), z(p_z), a(p_a) {}
+        vector4(const vector4 &v) : x(v.x), y(v.y), z(v.z), a(v.a) {}
+
+        vector4     operator-() const; //negative
+
+        vector4     operator+(const vector4& v) const;
+        vector4     operator-(const vector4& v) const;
+
+        float       length() const;
+        float       lengthsqr() const;
+
+        vector4     normalized() const;
+        void        normalize();
+
+        void        zero();
+        float       dot(const vector4& vecA, const vector4& vecB) const;
+        float       operator*(const vector4& v) const;
+        vector4     cross(const vector4& v) const;
+
+        vector4     operator*(float scalar) const;
+        vector4     operator/(float scalar) const;
+
+        float       x, y, z, a;
+    };
+
+
 
 #define DMG_VEC2 dmg::math::vector2
 #define DMG_VEC3 dmg::math::vector3
